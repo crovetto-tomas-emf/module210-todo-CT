f9f37bae97d5d4fdf9aa11b0e20c07e8318a1c9c
tomxssrt <tommycrovetto@bluewin.ch>
2026-02-05 11:26:40 +0100

    comit

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,5 +1,5 @@
 //const apiEndpoint = "https://fa-todo-backend-baqmes.azurewebsites.net/api/tasks";
-const apiEndpoint = "https://baqtestappbackend.blackglacier-c7b0844e.northeurope.azurecontainerapps.io/api/tasks";
+const apiEndpoint = "https://backend-210-fi21-crovetto-tomass.calmdesert-862d30c5.switzerlandnorth.azurecontainerapps.io/api/tasks";
 
 $(document).ready(function () {
   // Charger les tâches au démarrage
